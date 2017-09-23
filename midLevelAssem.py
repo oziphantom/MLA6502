e842124527831761b4fb7071c47f45119d62c429
oziphantom <oziphantom@hotmail.com>
2017-09-23 18:42:51 +1000

    pep8 fixes
    It also has an early >>= and <<= but I'm not sure about them and they
    need more optimisation work that will make sense to do after some
    refactoring.

--- midLevelAssem.py
+++ midLevelAssem.py
@@ -4,21 +4,25 @@ import tassDefineManager
 from enum import Enum
 from pyLog import pylog
 
-#inputFile = "D:\\GitHub\\SquidJump\\squid.asm"
-#outputFile = "D:\\GitHub\\SquidJump\\squid.psm"
-#inputFile = "D:/GitHub/test/midLevel.asm"
-#outputFile = "D:/GitHub/test/midLevel.psm"
+# inputFile = "D:\\GitHub\\SquidJump\\squid.asm"
+# outputFile = "D:\\GitHub\\SquidJump\\squid.psm"
+# inputFile = "D:/GitHub/test/midLevel.asm"
+# outputFile = "D:/GitHub/test/midLevel.psm"
+
+
 class MLASpecial(Enum):
     not_special = 0
     neg = 1
     pos = 2
     not_zero = 3
 
+
 class MLALineClass(Enum):
     straight_assign = 0
     if_comp = 1
     operator = 2
-    index_prehibitor = 3
+    index_prohibited = 3
+
 
 class MLAOperator(Enum):
     equal = 0
@@ -32,13 +36,17 @@ class MLAOperator(Enum):
     positive = 8
     negative = 9
     unknown = 128
-g_2operatorList = [MLAOperator.zero,MLAOperator.not_zero,MLAOperator.positive,MLAOperator.negative]
+
+
+g_2operatorList = [MLAOperator.zero, MLAOperator.not_zero, MLAOperator.positive, MLAOperator.negative]
+
 
 class MLAIndexPrehib(Enum):
     none = 0
     x = 1
     y = 2
     xy = 3
+
 
 class MLAPair:
     def __init__(self):
@@ -71,7 +79,7 @@ class MLAPair:
         if not self.src_immediate:
             if self.src_original.isnumeric():
                 if int(self.src_original) < 256:
-                    raise ValueError("non immediate assing with decimal - are you really sure? in line " + line)
+                    raise ValueError("non immediate assign with decimal - are you really sure? in line " + line)
             if "%" in self.src_original:
                 raise ValueError("Assign to % non immediate - are you really sure? in line " + line)
 
@@ -96,15 +104,14 @@ class MLAPair:
                 self.run = True
             elif count == 2:
                 if not (subs[1] == 'x' or
-                    subs[1] == 'y'):
-                        self.run = True
+                        subs[1] == 'y'):
+                    self.run = True
         if self.run:
             self.src = self.src_original
         else:    
             self.src = tdm.lookupValueFor(self.src_original)
 
         self.dest = tdm.lookupValueFor(self.dest_original)
-
 
 
 def is_valid_line(line):
@@ -122,6 +129,7 @@ def is_valid_line(line):
             return True
     return False
 
+
 def is_line_index_prehibit(line):
     safe = strip_lower_remove_comment(line)
     if safe == '!!x':
@@ -134,6 +142,7 @@ def is_line_index_prehibit(line):
         return True
     return False
 
+
 def strip_lower_remove_comment(line):
     local = line
     comment = line.find(';')
@@ -141,7 +150,8 @@ def strip_lower_remove_comment(line):
         local = line[:comment]
     return local.strip().lower()
 
-def get_prehib_enum_for_line(line):    
+
+def get_prehib_enum_for_line(line):
     safe = strip_lower_remove_comment(line)
     if safe == '!!x':
         return MLAIndexPrehib.x
@@ -153,6 +163,7 @@ def get_prehib_enum_for_line(line):
         return MLAIndexPrehib.xy
     return MLAIndexPrehib.none
 
+
 def get_index_letter_for_prehib(prehib):
     if prehib == MLAIndexPrehib.x:
         return "y"
@@ -160,7 +171,8 @@ def get_index_letter_for_prehib(prehib):
         return "x"
     if prehib == MLAIndexPrehib.none:
         return "x"
-    return "f" #for fail
+    return "f"  # for fail
+
 
 def get_line_class(line):
     if line.strip().startswith("!!if"):
@@ -170,24 +182,29 @@ def get_line_class(line):
             "&=" in line or
             "|=" in line or
             "^=" in line or
+            ">>=" in line or
+            "<<=" in line or
             "&|=" in line):
         return MLALineClass.operator
     if is_line_index_prehibit(line):
-        return MLALineClass.index_prehibitor
+        return MLALineClass.index_prohibited
     return MLALineClass.straight_assign
+
 
 def get_src_dest_from_line(line, tdm):
     pair = MLAPair()
     pair.make_pair(line, tdm)
     return pair
 
+
 def addPair(pairs, src, dest):
     if src in pairs:
-        dests = pairs[src] #already have this value, so append it
+        dests = pairs[src] # already have this value, so append it
         dests.append(dest)
         pairs[src] = dests
     else:
         pairs[src] = [dest] # just add it
+
 
 def convert_lines_to_asm(lines, segmentDM, tdm):
     out_string = []
@@ -199,23 +216,22 @@ def convert_lines_to_asm(lines, segmentDM, tdm):
             straights.append(line)
         elif clas == MLALineClass.if_comp:
             if len(straights) > 0:
-                out_string += convert_straight_assingment_block_to_asm(straights, segmentDM, tdm, prehib)
+                out_string += convert_straight_assignment_block_to_asm(straights, segmentDM, tdm, prehib)
                 straights = []
             out_string += (convert_if_line(line, segmentDM, tdm))
         elif clas == MLALineClass.operator:
             if len(straights) > 0:
-                out_string += convert_straight_assingment_block_to_asm(straights, segmentDM, tdm, prehib)
+                out_string += convert_straight_assignment_block_to_asm(straights, segmentDM, tdm, prehib)
                 straights = []
             out_string += convert_operator_line(line, segmentDM, tdm)
-        elif clas == MLALineClass.index_prehibitor:
+        elif clas == MLALineClass.index_prohibited:
             prehib = get_prehib_enum_for_line(line)
     if len(straights) > 0:
-        out_string += convert_straight_assingment_block_to_asm(straights, segmentDM, tdm, prehib)
-        straights = []
-
+        out_string += convert_straight_assignment_block_to_asm(straights, segmentDM, tdm, prehib)
     return out_string
 
-def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
+
+def convert_straight_assignment_block_to_asm(lines, segmentDM, tdm, prehib):
     # for each item we need to work out what is the src and the dest
     pairs = {}
     out_string = []
@@ -238,21 +254,21 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
         addPair(pairs, thing.src, thing.dest)
 
     for thing in words:
-        #is it immediate word or address
+        # is it immediate word or address
         if thing.src_immediate:
             local_src = str(thing.src)
             if local_src.startswith("#"):
                 local_src = local_src[1:]
-            #make lo
+            # make lo
             addPair(pairs, "#<"+local_src, thing.dest)
-            #make hi
+            # make hi
             addPair(pairs, "#>"+local_src, str(thing.dest)+"+1")
-        else: #nope its a word copy
+        else:  # nope its a word copy
             addPair(pairs, thing.src, thing.dest)
             addPair(pairs, str(thing.src)+"+1", str(thing.dest)+"+1")
 
     index_letter = get_index_letter_for_prehib(prehib)
-    #now we have a list of values that need to go to addresses
+    # now we have a list of values that need to go to addresses
     for thing in runs:
         split_key = thing.src.split(',')
         split_key_len = len(split_key)
@@ -265,7 +281,7 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
         line += "\tde"+index_letter+"\n"
         line += "\tbpl -\n"
         out_string.append(line)
-        #out.write(".segment data\nblockData_"+str(blockCounter)+"\t.byte "+key+"\n.send data\n")
+        # out.write(".segment data\nblockData_"+str(blockCounter)+"\t.byte "+key+"\n.send data\n")
         segmentDM.add_data("blockData_"+str(blockCounter), thing.src)
         segmentDM.inc_block_num()
 
@@ -278,13 +294,13 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
             if thing.special:
                 specials.append(thing)
             else:
-                addPair(pairs, thing.src, thing.dest) # nothing special we can do so add to dest
+                addPair(pairs, thing.src, thing.dest)  # nothing special we can do so add to dest
         else:
             if thing.src_immediate:
                 numbers.append(thing.src)
                 addPair(imm_pairs, thing.src, thing.dest)
             else:
-                addPair(pairs, thing.src, thing.dest) # nothing special we can do so add to dest
+                addPair(pairs, thing.src, thing.dest)  # nothing special we can do so add to dest
 
     for spec in specials:
         found = False
@@ -332,14 +348,14 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
     number_safe = []
 
     num_len = len(numbers)
-    if num_len > 0: # do we have just numbers to deal with
+    if num_len > 0:  # do we have just numbers to deal with
         if prehib == MLAIndexPrehib.xy:
             for key in imm_pairs:
                 pair = imm_pairs[key]
                 for d in pair:
                     addPair(pairs, "#"+str(key), str(d))
         else:
-            #make runs
+            # make runs
             i = 1
             start = numbers[0]
             current_run = [start]
@@ -377,7 +393,7 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
 
             num_runs.append(current_run)
 
-            #check for a FF->0 wrap run
+            # check for a FF->0 wrap run
             if len(num_runs) >= 2:
                 first = num_runs[0]
                 safe = number_safe[0]
@@ -391,7 +407,7 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
                             num_runs = num_runs[0:-1]
 
             for run in num_runs:
-                if len(run) == 1: #no index = no run
+                if len(run) == 1:  # no index = no run
                     pair = imm_pairs[run[0]]
                     for d in pair:
                         addPair(pairs, "#"+str(run[0]), str(d))
@@ -414,21 +430,23 @@ def convert_straight_assingment_block_to_asm(lines, segmentDM, tdm, prehib):
             out_string.append("\tsta "+str(dest)+"\n")
     return out_string
 
-def get_if_tokens3(line,operator):
+
+def get_if_tokens3(line, operator):
     then_split = line.split("then ")
     operator_split = then_split[0].split(operator)
     branch = then_split[1].strip()
     value = operator_split[1].strip()
     comparer = operator_split[0].strip()[4:]
-    return [comparer,value,branch]
+    return [comparer, value, branch]
 
-def get_if_tokens2(line,operator):
+
+def get_if_tokens2(line, operator):
     then_split = line.split("then ")
     operator_split = then_split[0].split(operator)
     branch = then_split[1].strip()
-    value = operator_split[1].strip()
     comparer = operator_split[0].strip()[4:]
     return [comparer, branch]
+
 
 def convert_if_line(line, segmentDM, tdm):
     operator = MLAOperator.unknown
@@ -440,7 +458,7 @@ def convert_if_line(line, segmentDM, tdm):
         macro = "#ISAEqualTo"
         if assemNumHelper.is_immediate(params[1]):
             value = tdm.lookupValueFor(params[1])
-            if isinstance(value,int):
+            if isinstance(value, int):
                 if value == 0:
                     pylog.write_log("converting == #0 to =0")
                     operator = MLAOperator.zero
@@ -448,11 +466,11 @@ def convert_if_line(line, segmentDM, tdm):
                     params[1] = params[2]
     elif "!=" in line:
         operator = MLAOperator.not_equal
-        params = get_if_tokens3(line,"!=")
+        params = get_if_tokens3(line, "!=")
         macro = "#ISANotEqualTo"
         if assemNumHelper.is_immediate(params[1]):
             value = tdm.lookupValueFor(params[1])
-            if isinstance(value,int):
+            if isinstance(value, int):
                 if value == 0:
                     pylog.write_log("converting != #0 to !0")
                     operator = MLAOperator.not_zero
@@ -460,11 +478,11 @@ def convert_if_line(line, segmentDM, tdm):
                     params[1] = params[2]
     elif ">=" in line:
         operator = MLAOperator.less_than
-        params = get_if_tokens3(line,">=")
+        params = get_if_tokens3(line, ">=")
         macro = "#ISAGTE"
         if assemNumHelper.is_immediate(params[1]):
             value = tdm.lookupValueFor(params[1])
-            if isinstance(value,int):
+            if isinstance(value, int):
                 if value == 128:
                     pylog.write_log("converting >= #128 to =+")
                     operator = MLAOperator.positive
@@ -477,11 +495,11 @@ def convert_if_line(line, segmentDM, tdm):
                     params[1] = params[2]
     elif "<=" in line:
         operator = MLAOperator.greater_than
-        params = get_if_tokens3(line,"<=")
+        params = get_if_tokens3(line, "<=")
         macro = "#ISALTE"
         if assemNumHelper.is_immediate(params[1]):
             value = tdm.lookupValueFor(params[1])
-            if isinstance(value,int):
+            if isinstance(value, int):
                 if value == 127:
                     pylog.write_log("converting <= #127 to =-")
                     operator = MLAOperator.negative
@@ -494,11 +512,11 @@ def convert_if_line(line, segmentDM, tdm):
                     params[1] = params[1]+"+1"
     elif ">" in line:
         operator = MLAOperator.greater_than_equal_to
-        params = get_if_tokens3(line,">")
+        params = get_if_tokens3(line, ">")
         macro = "#ISAGT"
         if assemNumHelper.is_immediate(params[1]):
             value = tdm.lookupValueFor(params[1])
-            if isinstance(value,int):
+            if isinstance(value, int):
                 if value == 127:
                     pylog.write_log("converting > #127 to =+")
                     operator = MLAOperator.positive
@@ -511,11 +529,11 @@ def convert_if_line(line, segmentDM, tdm):
                     params[1] = params[1]+"+1"
     elif "<" in line:
         operator = MLAOperator.less_than_equal_to
-        params = get_if_tokens3(line,"<")
+        params = get_if_tokens3(line, "<")
         macro = "#ISALT"
         if assemNumHelper.is_immediate(params[1]):
             value = tdm.lookupValueFor(params[1])
-            if isinstance(value,int):
+            if isinstance(value, int):
                 if value == 128:
                     pylog.write_log("converting < #128 to =-")
                     operator = MLAOperator.negative
@@ -523,26 +541,31 @@ def convert_if_line(line, segmentDM, tdm):
                     params[1] = params[2]                
     elif "=0" in line:
         operator = MLAOperator.zero
-        params = get_if_tokens2(line,"=0")
+        params = get_if_tokens2(line, "=0")
         macro = "#ISAZero"
     elif "!0" in line:
         operator = MLAOperator.not_zero
-        params = get_if_tokens2(line,"!0")
+        params = get_if_tokens2(line, "!0")
         macro = "#ISANotZero"
     elif "=+" in line:
         operator = MLAOperator.positive
-        params = get_if_tokens2(line,"=+")
+        params = get_if_tokens2(line, "=+")
         macro = "#ISAPlus"
     elif "=-" in line:
         operator = MLAOperator.negative
-        params = get_if_tokens2(line,"=-")
+        params = get_if_tokens2(line, "=-")
         macro = "#ISAMinus"
 
     if operator == MLAOperator.unknown:
-        print("unknown if statment found ",line)
-    elif operator in g_2operatorList: #2 operators
-        out_string = ["\t"+macro+" "+params[0]+","+params[1]+"\n"]
-    else: #3 operators
+        print("unknown if statment found ", line)
+    elif operator in g_2operatorList:  # 2 operators
+        if ",x" in params[0] or ",X" in params[0]:
+            out_string = ["\t"+macro+"iX "+params[0][:params[0].find(",")]+","+params[1]+"\n"]
+        elif ",y" in params[0] or ",Y" in params[0]:
+            out_string = ["\t"+macro+"iY "+params[0][:params[0].find(",")]+","+params[1]+"\n"]
+        else:
+            out_string = ["\t"+macro+" "+params[0]+","+params[1]+"\n"]
+    else:  # 3 operators
         if ",x" in params[0] or ",X" in params[0]:
             params[0] = params[0][:params[0].find(",")]
             macro += "iX"
@@ -562,17 +585,18 @@ def convert_if_line(line, segmentDM, tdm):
     return out_string
 
 
-def getOperatorTokens2(line,operator):
+def getOperatorTokens2(line, operator):
     parts = line.split(operator)
-    var = parts[0].strip()[2:] # remove the !!
+    var = parts[0].strip()[2:]  # remove the !!
     value = parts[1].strip()
     if ";" in value:
         value = value[0:value.find(";")].strip()
-    return [var,value]
+    return [var, value]
 
-def getOperatorTokens3(line,operator):
+
+def getOperatorTokens3(line, operator):
     parts = line.split(operator)
-    var = parts[0].strip()[2:] # remove the !!
+    var = parts[0].strip()[2:]  # remove the !!
     value = parts[1].strip()
     if ";" in value:
         value = value[0:value.find(";")].strip()
@@ -583,7 +607,7 @@ def getOperatorTokens3(line,operator):
     # v v x
     # v x v x
     if len(values) == 3:
-        #then we need to do some folding
+        # then we need to do some folding
         first = values[1].strip().lower()
         second = values[2].strip().lower()
         found = False
@@ -596,32 +620,38 @@ def getOperatorTokens3(line,operator):
             found = True
         if not found:
             pylog.write_log("invalid format for ",line)
-    elif  len(values) == 4:
+    elif len(values) == 4:
             values[0] = values[0]+","+values[1]
             values[1] = values[2]+","+values[3]
-    return [var,values[0],values[1]]
+    return [var, values[0], values[1]]
+
 
 def convert_operator_line(line, segmentDM, tdm):
     out_string = ""
     if "+=" in line:
-        parts = getOperatorTokens2(line,"+=")
+        parts = getOperatorTokens2(line, "+=")
         out_string = ["\tlda " + parts[0]+"\n\tclc\n\tadc "+parts[1]+"\n\tsta "+parts[0]+"\n"]
     elif "-=" in line:
-        parts = getOperatorTokens2(line,"-=")
+        parts = getOperatorTokens2(line, "-=")
         out_string = ["\tlda " + parts[0]+"\n\tsec\n\tsbc "+parts[1]+"\n\tsta "+parts[0]+"\n"]
     elif "&|=" in line:
-        parts = getOperatorTokens3(line,"&|=")
+        parts = getOperatorTokens3(line, "&|=")
         out_string = ["\tlda " + parts[0]+"\n\tand "+parts[1]+"\n\tora "+parts[2]+"\n\tsta "+parts[0]+"\n"]
     elif "&=" in line:
-        parts = getOperatorTokens2(line,"&=")
+        parts = getOperatorTokens2(line, "&=")
         out_string = ["\tlda " + parts[0]+"\n\tand "+parts[1]+"\n\tsta "+parts[0]+"\n"]
     elif "|=" in line:
-        parts = getOperatorTokens2(line,"|=")
+        parts = getOperatorTokens2(line, "|=")
         out_string = ["\tlda " + parts[0]+"\n\tora "+parts[1]+"\n\tsta "+parts[0]+"\n"]
     elif "^=" in line:
-        parts = getOperatorTokens2(line,"^=")
+        parts = getOperatorTokens2(line, "^=")
         out_string = ["\tlda " + parts[0]+"\n\teor "+parts[1]+"\n\tsta "+parts[0]+"\n"]    
-    
+    elif ">>=" in line:
+        parts = getOperatorTokens2(line, ">>=")
+        out_string = ["\tlda " + parts[0] + "\n\t.rept " + parts[1] + "\n\tlsr a\n\t.next\n\t sta " + parts[0]]
+    elif "<<=" in line:
+        parts = getOperatorTokens2(line, "<<=")
+        out_string = ["\tlda " + parts[0] + "\n\t.rept " + parts[1] + "\n\tasl a\n\t.next\n\t sta " + parts[0]]
     return out_string
 
 '''m_segmentDM = segmentDataManager.segmentDataManager()
